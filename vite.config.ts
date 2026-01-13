a7d5e624cb48335610afbc5f977c414accb528d0
Olivernoeru <shafarifaiii@gmail.com>
2026-01-13 13:00:00 +0700

    Deploy project to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/fashionx-ecommerce-matematika-diskrit/",
   server: {
     host: "::",
     port: 8080,
